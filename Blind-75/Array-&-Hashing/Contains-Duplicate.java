2187001a81c9ef39b9995a168fc3174f09a16d3e
galiannm <galiannm@tcd.ie>
2025-06-19 16:08:29 +0700

    Added problem 217

---
+++ Blind-75/Array-&-Hashing/Contains-Duplicate.java
@@ -0,0 +1,16 @@
+import java.util.HashSet;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) 
+    {
+        HashSet<Integer> seen = new HashSet<>();
+        for (int i = 0; i < nums.length; i++)
+        {
+            if (seen.contains(nums[i]))
+                return true;
+            else
+                seen.add(nums[i]);
+        }
+        return false;
+    }
+}
