f9637a177564e22def99cfb764fc10ab92c1c089
galiannm <galiannm@tcd.ie>
2025-06-28 15:02:06 +0100

    Added problem 88

--- OtherProblems/Merge-Sorted-Array.py
+++ OtherProblems/Merge-Sorted-Array.py
@@ -3,14 +3,14 @@ from typing import List
 
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        r, l, i = m+n-1, m-1, n-1
-        while i >= 0: 
-            if l >= 0 and nums1[l] > nums2[i]:
-                nums1[r] = nums1[l]
-                l -= 1
+        r, m_l, n_l = m+n-1, m-1, n-1
+        while n_l >= 0: 
+            if m_l >= 0 and nums1[m_l] > nums2[n_l]:
+                nums1[r] = nums1[m_l]
+                m_l -= 1
             else:
-                nums1[r] = nums2[i]
-                i -= 1
+                nums1[r] = nums2[n_l]
+                n_l -= 1
             r -= 1
 
 
