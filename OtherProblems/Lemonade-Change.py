87de9adc61ac784baa2d97e28cb597c28d36e0f3
galiannm <galiannm@tcd.ie>
2025-09-15 21:39:21 +0100

    Added problem 860

---
+++ OtherProblems/Lemonade-Change.py
@@ -0,0 +1,22 @@
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        # 5, 10, 20 bills
+        # price = 5
+
+        fives, tens = 0, 0
+        for b in bills:
+            if b == 5:
+                fives += 1
+            elif b == 10:
+                if fives <= 0: return False
+                tens += 1
+                fives -= 1
+            else:
+                if tens > 0 and fives > 0: 
+                    tens -= 1
+                    fives -= 1
+                elif fives >= 3: 
+                    fives -= 3
+                else: return False
+        return True
+                
