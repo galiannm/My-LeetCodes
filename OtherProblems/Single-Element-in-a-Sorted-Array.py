6f0e07f12f52f23991306bd6becd773cf55e763e
galiannm <galiannm@tcd.ie>
2025-09-25 10:55:55 +0100

    Added problem 540

---
+++ OtherProblems/Single-Element-in-a-Sorted-Array.py
@@ -0,0 +1,14 @@
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        lo, hi = 0, len(nums)-1
+        while lo < hi: 
+            mid = lo + (hi - lo)//2
+            if mid%2 == 1: 
+                mid -= 1
+            if nums[mid] == nums[mid+1]:
+                lo = mid + 2
+            else:
+                hi = mid
+        return nums[lo]
